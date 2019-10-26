ad98eb9224f6db469970a1dc27757bef7feb91a6
IanMcCormick <IMMcC4486@yahoo.com>
2019-10-26 14:14:12 -0400

    who knows

---
+++ Project2/src/P1/Hello.java
@@ -0,0 +1,15 @@
+package P1;
+
+public class Hello {
+
+	
+	public static void main(String[] args) {
+		int a = 45;
+		float b = 54;
+		System.out.println(45 + 54);
+		System.out.println(a + b);
+		String c = "1";
+		System.out.println(a + c);
+	}
+	
+}
